3a2ab9b4c895accc7a9b6f31bbd36ce69c3820e1
setsu556 <qq5y5bsd@gmail.com>
2025-10-15 16:44:45 +0900

    アプリケーションの基本的な構造と機能を追加

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('Vue.js Course')
+let price = ref(9.99)
+function increment() {
+  price.value += 1
+}
+const info = ref({
+  students: 1000,
+  rating: 4,
+})
+console.log(info.value.students)
+</script>
+<template>
+  <h1>Title: {{ title }}</h1>
+  <h2>Price: ${{ price - 1 }}</h2>
+  <button @click="increment">button</button>
+  <h2>Students: {{ info.students }}</h2>
+</template>
